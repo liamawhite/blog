158b868edf2f4cb4cc99d8c23091f644051f27e4
Liam White <liam@tetrate.io>
2021-04-12 12:15:11 -0700

    Add talks functionality

---
+++ static/reveal/plugin/mermaid/plugin.js
@@ -0,0 +1,27 @@
+window.RevealMermaid = window.RevealMermaid || {
+    id: 'RevealMermaid',
+    init: function(deck) {
+        initMermaid(deck);
+    }
+};
+
+const initMermaid = function(Reveal){
+    Reveal.addEventListener('ready', function(event){
+        Reveal.getSlides().forEach(slide => {
+            console.log(slide)
+            slide.querySelectorAll("code.mermaid").forEach(m => {
+                // Use div instead of pre.code to avoid reveal CSS
+                let newDiv = document.createElement("div")
+                newDiv.classList.add('mermaid')
+                m.parentNode.parentNode.replaceChild(newDiv, m.parentNode)
+                
+                // Mermaid expects just the text, reveal adds spans to code blocks
+                newDiv.innerHTML = m.innerText
+    
+                // Center the diagram
+                newDiv.style.display = 'flex'
+                newDiv.style.justifyContent = 'center'
+            })
+        })
+    })
+}
